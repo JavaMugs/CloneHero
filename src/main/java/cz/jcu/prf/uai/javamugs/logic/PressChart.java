3f80f286a4c8648f439f85bfeffcf1e0c8b3a3f1
Pavel Šeps <papaja54@gmail.com>
2017-07-04 12:14:02 +0200

    Chord class, Press class, PressChart class

---
+++ src/main/java/cz/jcu/prf/uai/javamugs/logic/PressChart.java
@@ -0,0 +1,17 @@
+package cz.jcu.prf.uai.javamugs.logic;
+
+import java.util.ArrayList;
+
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+
+public class PressChart {
+
+	public PressChart(ArrayList<Press> presses) {
+		throw new NotImplementedException();
+	}
+	
+	public Chord next(double currentTime) {
+		throw new NotImplementedException();
+	}
+}
