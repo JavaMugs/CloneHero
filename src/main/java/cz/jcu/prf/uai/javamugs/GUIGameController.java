a2ccce84229d03d5cbc5ff904c9b25308175f2dd
Ivan Kratochvíl <ivan.kratochvil@layoutmaschine.de>
2017-07-04 11:32:10 +0200

    GUI

---
+++ src/main/java/cz/jcu/prf/uai/javamugs/GUIGameController.java
@@ -0,0 +1,14 @@
+package cz.jcu.prf.uai.javamugs;
+
+import javafx.scene.control.Button;
+
+import javafx.event.ActionEvent;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+
+import java.io.File;
+
+public class GUIGameController {
+
+}
+
