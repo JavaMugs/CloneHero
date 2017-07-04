f458669e8f49d3029fd104bbbaf42e0f33ef5b66
Ivan Kratochvíl <ivan.kratochvil@layoutmaschine.de>
2017-07-04 09:10:01 +0200

    Initial

---
+++ src/main/java/cz/jcu/prf/uai/javamugs/App.java
@@ -0,0 +1,13 @@
+package cz.jcu.prf.uai.javamugs;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
