2e6d4acb5b0b66155bb36fa1ce50a4590451b31a
ivankrato <ivankrato2@gmail.com>
2017-07-05 08:52:43 +0200

    Buffer class implementation

--- src/main/java/cz/jcu/prf/uai/javamugs/logic/Buffer.java
+++ src/main/java/cz/jcu/prf/uai/javamugs/logic/Buffer.java
@@ -19,13 +19,14 @@ public class Buffer {
 
     /**
      * Creates Buffer with set difficulty
+     *
      * @param difficulty 1-3 - tolerance of hits and misses
      */
     public Buffer(byte difficulty) {
         chordQueue = new LinkedList<Chord>();
         pressTimes = new ArrayList<Double>();
         this.difficulty = difficulty;
-        switch(difficulty) {
+        switch (difficulty) {
             case 1:
                 this.tolerance = 150;
                 break;
@@ -41,7 +42,8 @@ public class Buffer {
 
     /**
      * Adds chord to queue and time to list.
-     * @param chord Chord to add to buffer
+     *
+     * @param chord     Chord to add to buffer
      * @param pressTime time to when the chord should be pressed
      */
     public void addToBuffer(Chord chord, double pressTime) {
@@ -50,15 +52,42 @@ public class Buffer {
     }
 
     /**
-     * Checks if time exists in list and dequeue Chords from queue if so
+     * Checks if time exists in list and dequeue Chords from queue if so. Cannot be null.
+     *
      * @param pressedKeys Chord of pressed keys from
-     * @param PressTime Time of the press
+     * @param pressTime   Time of the press
      * @return Pair of hits and misses
      */
     public BufferReport check(Chord pressedKeys, double pressTime) {
-        double minTime = pressTime - 150;
-        double maxTime = pressTime + 150;
-        throw new NotImplementedException();
+        double minTime = pressTime - tolerance;
+        double maxTime = pressTime + tolerance;
+        double chordTime = -1;
+        for (int i = 0; i < pressTimes.size(); i++) {
+            double time = pressTimes.get(i);
+            if (time > minTime && time < maxTime) {
+                chordTime = time;
+                pressTimes.remove(i);
+                break;
+            }
+        }
+        Chord expectedChord;
+        if (chordTime > 0) {
+            expectedChord = chordQueue.poll();
+        } else {
+            expectedChord = new Chord(false, false, false, false, false);
+        }
+        int hits = 0;
+        int misses = 0;
+        for (int i = 0; i < 5; i++) {
+            if (pressedKeys.getChords()[i] && !expectedChord.getChords()[i] // key pressed, nothing expected
+                    || !pressedKeys.getChords()[i] && !expectedChord.getChords()[i]) { // nothing pressed, key press expected
+                misses++;
+            }
+            if (pressedKeys.getChords()[i] && expectedChord.getChords()[i]) {
+                hits++;
+            }
+        }
+        return new BufferReport(hits, misses, expectedChord);
     }
 
     /**
