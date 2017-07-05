76072bde1c1b3359a45356f333c2798c707e31f9
Pavel Šeps <pavel.seps@layoutmaschine.de>
2017-07-05 08:54:03 +0200

    Comment Chord, Press, PressChart class + make PressChart class

--- src/main/java/cz/jcu/prf/uai/javamugs/logic/Press.java
+++ src/main/java/cz/jcu/prf/uai/javamugs/logic/Press.java
@@ -5,16 +5,27 @@ public class Press {
 	
 	private int color;
 	private double drawTime;
-			
+
+	/**
+	 * Constructor, set color and drawTime
+	 * @param color color of chord
+	 * @param drawTime time when chord will be draw
+	 */
 	public Press(int color, double drawTime) {
 		this.color = color;
 		this.drawTime = drawTime;
 	}
-	
+
+	/**
+	 * @return color of chord
+	 */
 	public int getColor() {
 		return this.color;
 	}
-	
+
+	/**
+	 * @return draw time on screen
+	 */
 	public double getDrawTime() {
 		return this.drawTime;
 	}
